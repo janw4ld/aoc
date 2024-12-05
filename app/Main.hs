32726813ce375271604a06f79b449f3dcc4b41e2
janw4ld <janw4ld@tutanota.com>
2024-12-05 08:16:47 +0200

    day4 part1 in haskell

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,9 @@ module Main (main) where
 
 import Control.Monad
 import Data.Bifunctor
+import Data.Functor
 import Data.List
+import Data.Maybe
 import Debug.Trace
 
 input = readFile "./test-input"
@@ -11,25 +13,44 @@ input = readFile "./test-input"
 -- input = readFile "./input"
 solve = part1 . prepInput
 
-part1 = id
+part1 grid =
+  sum
+    [ scanForXmas grid x y
+    | (y, line) <- zip [0 ..] grid
+    , (x, char) <- zip [0 ..] line
+    , char == anchor
+    ]
+ where
+  anchor = 'X'
 
 part2 = id
 
-prepInput = id
+prepInput = lines
 
-splitOn delim str
-  | null str = [[]]
-  | delim `isPrefixOf` str = [] : splitOn delim (drop (length delim) str)
-  | otherwise =
-      let (h : rest) = splitOn delim (tail str)
-       in (head str : h) : rest
-zipAdjacentWith f xs = zipWith f xs (tail xs)
+scanForXmas grid x y =
+  length
+    . filter id
+    . fromMaybe []
+    . sequence
+    $ [ (grid !? y' >>= (!? x')) <&> (== c)
+      | (c, s) <- steps
+      , (dx, dy) <- deltas
+      , let
+          x' = x + dx * s
+          y' = y + dy * s
+      ]
+ where
+  deltas = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+  steps = [('M', 1), ('A', 2), ('S', 3)]
+
+zipAdjacentWith f xs = zipWith f xs (drop 1 xs)
 markSignChanges = zipAdjacentWith (\x y -> x * y < 0)
 diffEachTwo = zipAdjacentWith (-)
 groupByParity = \case
   [] -> ([], [])
   (x : xs) -> let (odds, evens) = groupByParity xs in (x : evens, odds)
 
+rotate n xs = take (length xs) $ drop n $ cycle xs
 deleteAt xs n = take n xs <> drop (n + 1) xs
 trace1 v = trace (show v) v
 infixl 0 |>
